6e025bf362837c3912c5d4f05921ca026d801f57
yeosong613 <104603242+yeosong613@users.noreply.github.com>
2022-05-11 15:01:25 +0900

    Add files via upload

--- trm02/js/main.js
+++ trm02/js/main.js
@@ -40,5 +40,39 @@ $(function () {
         $('myMovie').YTPPause();
     });
 
+    $('.product_slider').on('init reInit afterChange', function () {
+        let current = $('product_slider .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    })
+
+    $('.product_slider').slick({
+        slidesToShow: 5,
+        centerMode: true,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    });
+
+    $('.product_list .s_left').on('click', function () {
+        $('.product_slider').slick('slickPrev');
+    })
+
+    $('.product_list .s_right').on('click', function () {
+        $('.product_slider').slick('slickNext');
+    })
+
+    $('.tab_link>li').on('click', function () {
+        var idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        $('.tab_content>li').eq(idx).addClass('on').siblings().removeClass('on');
+    })
+
+    $('.footer #link').on('change', function () {
+        var lik = $(this).val();
+        if (lik) window.open(lik)
+    })
+
+
+
     /////////////////////////////////////////////////
 });
